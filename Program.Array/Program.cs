88d32013105991ff620e63f7f8bd644d7a2e66ae
Константин <kostay_mc@mail.ru>
2022-12-27 20:26:47 +0300

    creat program

---
+++ Program.Array/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] Massiv(int size)
+{
+    Console.WriteLine("Введите слова");
+    string [] array = new string [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine();    
+    }
+    return array;
+}
+
+string Choice(string [] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        if(i < 
+    }
+}
+
+Console.WriteLine("Введите количесто слов");
+string [] array1 = Massiv(int.Parse(Console.ReadLine()));
